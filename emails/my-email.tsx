04e99ee8125f28bfea9e190da6b708addf4da643
Youri Westerman <y.westerman@amsterdam.nl>
2025-10-24 11:57:30 +0200

    Add email template

---
+++ emails/my-email.tsx
@@ -0,0 +1,71 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Hr,
+  Img,
+  Link,
+  Markdown,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+import { baseUrl } from "../src/config";
+
+interface EmailProps {
+  title: string;
+  previewText: string;
+  bodyText: string;
+  imageSrc: string;
+}
+
+export default function Email(props: EmailProps) {
+  return (
+    <Html>
+      <Head>
+        <title>{props.title}</title>
+      </Head>
+      <Body>
+        <Preview>{props.previewText}</Preview>
+        <Container>
+          {/* Header */}
+          <Section>
+            <Img
+              src={props.imageSrc}
+              width="115"
+              height="40"
+              alt="Gemeente Amsterdam Logo"
+            />
+          </Section>
+          {/* Body */}
+          <Section>
+            <Heading>{props.title}</Heading>
+            <Markdown>{props.bodyText}</Markdown>
+          </Section>
+          <Hr />
+          {/* Disclaimer */}
+          <Section>
+            <Text>Disclaimer teksten zijn altijd superleuk om te lezen!</Text>
+          </Section>
+          {/* Footer */}
+          <Text>
+            <Link href="https://amsterdam.nl/">Amsterdam.nl</Link>
+          </Text>
+          <Text>
+            <Link href="tel:14020">14020</Link>
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+}
+
+Email.PreviewProps = {
+  title: "My Email Title",
+  previewText: "A nice preview text for mail clients",
+  bodyText: "A quick brown fox jumps over the lazy dog.",
+  imageSrc: `${baseUrl}/amsterdam-logo.png`,
+};
