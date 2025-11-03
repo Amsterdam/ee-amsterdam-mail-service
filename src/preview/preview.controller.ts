ba1ba239805302dd28a3454e8017ee3ec4bc8bfd
Youri Westerman <y.westerman@amsterdam.nl>
2025-11-03 09:39:23 +0100

    Make sure http status code 200 is returned

--- src/preview/preview.controller.ts
+++ src/preview/preview.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { PreviewRequestDto } from './preview.dto.js';
 import { ApiOkResponse } from '@nestjs/swagger';
 import PreviewRenderer from './preview.js';
@@ -8,6 +8,7 @@ export class PreviewController {
   public constructor(private readonly renderer: PreviewRenderer) {}
 
   @Post()
+  @HttpCode(200)
   @ApiOkResponse({
     description: 'OK',
     content: {
