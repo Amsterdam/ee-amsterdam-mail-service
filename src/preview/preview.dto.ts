3ee29549c22fb09de1d77527eb3261d4a7dc1529
Youri Westerman <y.westerman@amsterdam.nl>
2025-11-03 13:49:54 +0100

    Ignore typing issues in DTO

--- src/preview/preview.dto.ts
+++ src/preview/preview.dto.ts
@@ -4,13 +4,16 @@ import { IsString } from 'class-validator';
 export class PreviewRequestDto {
   @ApiProperty()
   @IsString()
+  // @ts-ignore TS2564
   title: string;
 
   @ApiProperty()
   @IsString()
+  // @ts-ignore TS2564
   previewText: string;
 
   @ApiProperty()
   @IsString()
+  // @ts-ignore TS2564
   bodyText: string;
 }
