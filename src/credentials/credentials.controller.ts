e4136d3bf748021189e1351e5f5f690804d72e00
Youri Westerman <y.westerman@amsterdam.nl>
2025-11-12 13:41:52 +0100

    Implement controller method

--- src/credentials/credentials.controller.ts
+++ src/credentials/credentials.controller.ts
@@ -1,5 +1,10 @@
-import { Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { CredentialsUpserter } from './credentials';
+import {
+  CredentialsRequestDto,
+  CredentialsResponseDto,
+} from './credentials.dto';
+import { ApiBody, ApiResponse } from '@nestjs/swagger';
 
 @Controller('credentials')
 export class CredentialsController {
@@ -7,7 +12,20 @@ export class CredentialsController {
 
   @Post()
   @HttpCode(200)
-  public async credentials() {
-    return 'HELLO';
+  @ApiBody({ type: CredentialsRequestDto })
+  @ApiResponse({ type: CredentialsResponseDto })
+  public async credentials(
+    @Body() credentialsRequestDto: CredentialsRequestDto,
+  ): Promise<CredentialsResponseDto> {
+    await this.upserter.upsert(
+      'abc', // TODO: Get this value from access token
+      credentialsRequestDto.username,
+      credentialsRequestDto.password,
+    );
+
+    const response = new CredentialsResponseDto();
+    response.message = 'SMTP credentials successfully stored in keyvault!';
+
+    return response;
   }
 }
