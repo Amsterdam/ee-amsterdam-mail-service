c8093440be2c1f14456720681da1acd412be2498
Youri Westerman <y.westerman@amsterdam.nl>
2025-11-17 15:12:38 +0100

    Set up okta jwt verifier

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import PreviewRenderer from './preview/preview';
 import Joi from 'joi';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import OktaJwtVerifier, { type VerifierOptions } from '@okta/jwt-verifier';
+import { JWKSUriResolver } from './auth';
 
 @Module({
   imports: [
@@ -30,15 +32,22 @@ import { join } from 'path';
           })
           .required(),
         PORT: Joi.number().port().default(3001),
-        OIDC_ISSUER: Joi.string().uri().default(
-          'http://localhost:8002/realms/amsterdam-mail-service',
-        ),
+        OIDC_ISSUER: Joi.string()
+          .uri()
+          .default('http://localhost:8002/realms/amsterdam-mail-service'),
         OIDC_AUDIENCE: Joi.string().default('amsterdam-mail-service'),
         OIDC_ALGORITHMS: Joi.string().default('RS256,RS384,RS512'),
         OIDC_DISCOVERY_URL: Joi.string()
           .uri()
-          .default('http://localhost:8002/realms/amsterdam-mail-service/.well-known/openid-configuration'),
+          .default(
+            'http://keycloak:8002/realms/amsterdam-mail-service/.well-known/openid-configuration',
+          ),
         OIDC_CLIENT_ID: Joi.string().default('amsterdam-mail-service'),
+        SWAGGER_UI_OIDC_DISCOVERY_URL: Joi.string()
+          .uri()
+          .default(
+            'http://localhost:8002/realms/amsterdam-mail-service/.well-known/openid-configuration',
+          ),
       }),
     }),
     ServeStaticModule.forRoot({
@@ -55,6 +64,42 @@ import { join } from 'path';
         const baseUrl = configuration.get<string>('APP_BASE_URL');
         // @ts-expect-error TS2345
         return new PreviewRenderer(baseUrl);
+      },
+    },
+    {
+      provide: JWKSUriResolver,
+      inject: [ConfigService],
+      useFactory: (configuration: ConfigService): JWKSUriResolver => {
+        return new JWKSUriResolver(
+          // @ts-expect-error TS2345
+          configuration.get<string>('OIDC_DISCOVERY_URL'),
+        );
+      },
+    },
+    {
+      provide: OktaJwtVerifier,
+      inject: [ConfigService, JWKSUriResolver],
+      useFactory: async (
+        configuration: ConfigService,
+        jwksUriResolver: JWKSUriResolver,
+      ): Promise<OktaJwtVerifier> => {
+        const jwksUri = await jwksUriResolver.resolve();
+
+        const verifierOptions: VerifierOptions = {
+          // @ts-expect-error TS2345
+          issuer: configuration.get<string>('OIDC_ISSUER'),
+          jwksUri: jwksUri,
+        };
+
+        // Disable the https check for environments other than 'production'
+        const nodeEnv = configuration.get<string>('NODE_ENV');
+        if (nodeEnv !== 'production') {
+          verifierOptions.testing = {
+            disableHttpsCheck: true,
+          };
+        }
+
+        return new OktaJwtVerifier(verifierOptions);
       },
     },
   ],
