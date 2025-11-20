ba012563fae063c52007a61af37497302f7f256e
Youri Westerman <y.westerman@amsterdam.nl>
2025-11-20 15:38:46 +0100

    Add factories in order to get a `Sender` into the controller

--- src/send/send.tsx
+++ src/send/send.tsx
@@ -1,9 +1,63 @@
-import type { Transporter } from 'nodemailer';
+import nodemailer, { type Transporter } from 'nodemailer';
 import Email from '../emails/my-email';
 import React from 'react';
 import { render, toPlainText } from '@react-email/render';
 import fs from 'fs';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+import type CredentialsRepository from 'src/repositories';
+
+export class TransporterFactory {
+  public constructor(
+    private smtpHost: string,
+    private smtpPort: number,
+  ) {}
+
+  public produce(
+    smtpUsername: string,
+    smtpPassword: string,
+  ): Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options> {
+    return nodemailer.createTransport(
+      `smtp://${this.smtpHost}:${this.smtpPort}`,
+      {
+        auth: {
+          user: smtpUsername,
+          pass: smtpPassword,
+        },
+      },
+    );
+  }
+}
+
+export class MailerFactory {
+  public constructor(
+    private credentialsRepository: CredentialsRepository,
+    private transporterFactory: TransporterFactory,
+  ) {}
+
+  public async produce(sub: string): Promise<Mailer> {
+    const credentials =
+      await this.credentialsRepository.getCredentialsBySub(sub);
+
+    return new Mailer(
+      this.transporterFactory.produce(
+        credentials.smtpUsername,
+        credentials.smtpPassword,
+      ),
+    );
+  }
+}
+
+export class SenderFactory {
+  public constructor(
+    private renderer: Renderer,
+    private mailerFactory: MailerFactory,
+  ) {}
+
+  public async produce(sub: string): Promise<Sender> {
+    const mailer = await this.mailerFactory.produce(sub);
+    return new Sender(this.renderer, mailer);
+  }
+}
 
 interface Rendered {
   html: string;
