d4b74620a75df8f027723726357fa3a5f4ef0f52
Youri Westerman <y.westerman@amsterdam.nl>
2025-11-20 15:39:13 +0100

    Add DTOs

---
+++ src/send/send.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
+export class SendRequestDto {
+  @ApiProperty()
+  @IsString()
+  // @ts-expect-error TS2564
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  // @ts-expect-error TS2564
+  previewText: string;
+
+  @ApiProperty()
+  @IsString()
+  // @ts-expect-error TS2564
+  bodyText: string;
+
+  @ApiProperty()
+  @IsEmail()
+  // @ts-expect-error TS2564
+  from: string;
+
+  @ApiProperty()
+  @IsEmail()
+  // @ts-expect-error TS2564
+  to: string;
+
+  @ApiProperty()
+  @IsString()
+  // @ts-expect-error TS2564
+  subject: string;
+}
+
+export class SendResponseDto {
+  @ApiProperty()
+  @IsString()
+  // @ts-expect-error TS2564
+  message: string;
+}
+
+export class SendResponseDtoFactory {
+  public produce(message: string): SendResponseDto {
+    const dto = new SendResponseDto();
+    dto.message = message;
+
+    return dto;
+  }
+}
